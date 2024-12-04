3e05dd946fea384224b95113ffb90f938373f693
gaby <83096325+gabs18@users.noreply.github.com>
2024-12-04 15:25:38 -0600

    Agregar introducción

--- felinos.py
+++ felinos.py
@@ -37,7 +37,16 @@ felinos_gdf = cargar_gpkg_felinos()
 areas_gdf = cargar_datos_areas()
 
 # Configuración de la app
-st.title("Datos de felinos y Áreas de Conservación en Costa Rica")
+st.title("Avistamientos de felinos en las Áreas de Conservación de Costa Rica")
+
+# Introducción
+st.markdown("""
+    ---
+    ### Introducción
+    Costa Rica es conocida por su rica biodiversidad, alberga varias especies de felinos que desempeñan un papel esencial en el equilibrio ecológico. Desde el jaguar hasta el ocelote, estos felinos se encuentran en diferentes ecosistemas del país. Sin embargo, enfrentan desafíos significativos debido a la pérdida de hábitat, la caza furtiva y otros factores antropogénicos.
+    
+    Este sitio web tiene como objetivo proporcionar una visión de los registros de observaciones de tres especies de felinos en Costa Rica en cada Área de Conservación. Se incluye una barra lateral para filtrar los registros por Área de Conservación, al interactuar con ella, la tabla, el gráfico paste y el mapa se van a actualizar para mostrar sólo la información de dicha área de conservación.
+    """)
 
 # Ruta a la imagen (ajusta la ruta si está en una subcarpeta)
 imagen_path = "datos/foto.png"  # Reemplaza con el nombre de tu archivo de imagen
@@ -198,7 +207,7 @@ else:
 
 
 
-# Autoría de la páigna
+# Autoría de la página
 st.markdown("""
     ---
     ### Créditos
